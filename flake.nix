84b30a3e0a6c1ae2bc6c82952628128214df8593
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-03-28 13:07:57 +0000

    add formatter for nix files

--- flake.nix
+++ flake.nix
@@ -1,9 +1,26 @@
 {
   description = "NixOS/Home-Manager Config Flake";
 
-  inputs = {};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
 
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    lib = nixpkgs.lib;
+    systems = ["x86_64-linux" "aarch64-linux"];
 
-  outputs = inputs@{ self, ... }: {};
+    pkgsFor = lib.genAttrs systems (system:
+      import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      });
 
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+  in {
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
+  };
 }
