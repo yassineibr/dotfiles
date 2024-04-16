8227328a56c07ccaab2e11971b08d4427f311ceb
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-16 10:51:15 +0100

    add wlogout for gui desktops

---
+++ home/yassine/common/optional/gui/others/wlogout/default.nix
@@ -0,0 +1,111 @@
+{config, ...}: {
+  programs.wlogout = {
+    enable = true;
+    layout = [
+      {
+        label = "lock";
+        action = "sleep 1 && grim '/home/yassine/.cache/lockscreen.png' && hyprlock";
+        text = "Lock";
+        keybind = "l";
+      }
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "r";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+      }
+      {
+        label = "logout";
+        action = "hyprctl dispatch exit 0";
+        text = "Logout";
+        keybind = "e";
+      }
+      {
+        label = "suspend";
+        action = "sleep 1 && grim '/home/yassine/.cache/lockscreen.png' && systemctl suspend && hyprlock";
+        text = "Suspend";
+        keybind = "u";
+      }
+      {
+        label = "windows";
+        action = "winboot";
+        text = "Windows";
+        keybind = "w";
+      }
+    ];
+    style = ''
+         window {
+      font-family: monospace;
+      font-size: 14pt;
+      color: #cdd6f4; /* text */
+      background-color: rgba(30, 30, 46, 0.5);
+         }
+
+         button {
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: 25%;
+      border: none;
+      background-color: rgba(30, 30, 46, 0);
+      margin: 5px;
+      transition: box-shadow 0.2s ease-in-out, background-color 0.2s ease-in-out;
+         }
+
+         button:hover {
+      background-color: rgba(49, 50, 68, 0.1);
+         }
+
+         button:focus {
+      background-color: #cba6f7;
+      color: #1e1e2e;
+         }
+
+         #lock {
+      background-image: image(url("${./icons/lock.png}"));
+         }
+         #lock:focus {
+      background-image: image(url("${./icons/lock-hover.png}"));
+         }
+
+         #logout {
+      background-image: image(url("${./icons/logout.png}"));
+         }
+         #logout:focus {
+      background-image: image(url("${./icons/logout-hover.png}"));
+         }
+
+         #suspend {
+      background-image: image(url("${./icons/sleep.png}"));
+         }
+         #suspend:focus {
+      background-image: image(url("${./icons/sleep-hover.png}"));
+         }
+
+         #shutdown {
+      background-image: image(url("${./icons/power.png}"));
+         }
+         #shutdown:focus {
+      background-image: image(url("${./icons/power-hover.png}"));
+         }
+
+         #reboot {
+      background-image: image(url("${./icons/restart.png}"));
+         }
+         #reboot:focus {
+      background-image: image(url("${./icons/restart-hover.png}"));
+         }
+         #windows {
+      background-image: image(url("${./icons/windows.png}"));
+         }
+         #windows:focus {
+      background-image: image(url("${./icons/windows-hover.png}"));
+         }
+    '';
+  };
+}
