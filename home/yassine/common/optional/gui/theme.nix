67399d4afd4a82433e903441f318c29e759e7fae
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-16 11:33:38 +0100

    add gtk and qt theming

---
+++ home/yassine/common/optional/gui/theme.nix
@@ -0,0 +1,62 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
+
+  home.sessionVariables = {
+    GTK_THEME = config.gtk.theme.name;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+
+    iconTheme.package = pkgs.gnome.adwaita-icon-theme;
+    iconTheme.name = "Adwaita";
+
+    gtk3 = {
+      extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
+    };
+
+    gtk4 = {
+      extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
+    };
+  };
+
+  qt = {
+    enable = true;
+    # platform theme "gtk" or "gnome"
+    platformTheme = "gnome";
+
+    # name of the qt theme
+    style.name = "Adwaita-dark";
+
+    # detected automatically:
+    # adwaita, adwaita-dark, adwaita-highcontrast,
+    # adwaita-highcontrastinverse, breeze,
+    # bb10bright, bb10dark, cde, cleanlooks,
+    # gtk2, motif, plastique
+
+    # package to use
+    style.package = pkgs.adwaita-qt;
+  };
+}
