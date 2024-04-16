0337768774e908dae4fd89198fab50c79ad014f2
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-16 11:00:28 +0100

    add waybar

---
+++ home/yassine/common/optional/gui/bars/waybar/default.nix
@@ -0,0 +1,146 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  programs.waybar = {
+    enable = true;
+    systemd = {
+      enable = false;
+      target = "graphical-session.target";
+    };
+    style = ./style.css;
+    settings = [
+      {
+        "layer" = "top";
+        "position" = "top";
+        modules-left = [
+          "custom/launcher"
+          "temperature"
+          "hyprland/workspaces"
+        ];
+        modules-center = [
+          "clock"
+        ];
+        modules-right = [
+          "network"
+          "memory"
+          "cpu"
+          "pulseaudio"
+          "pulseaudio#microphone"
+          "backlight"
+          "battery"
+          "custom/powermenu"
+          "tray"
+        ];
+        "hyprland/workspaces" = {
+          disable-scroll = false;
+          all-outputs = false;
+          on-click = "activate";
+          show-special = false;
+        };
+
+        "custom/launcher" = {
+          "format" = " ";
+          "on-click" = "pkill rofi || rofi -show drun -show-icons";
+          "on-click-middle" = "exec default_wall";
+          "on-click-right" = "exec wallpaper_random";
+          "tooltip" = false;
+        };
+        "custom/cava-internal" = {
+          "exec" = "sleep 1s && cava-internal";
+          "tooltip" = false;
+        };
+        "pulseaudio" = {
+          "scroll-step" = 1;
+          "format" = "{icon} {volume}%";
+          "format-muted" = "󰖁 Muted";
+          "format-icons" = {
+            "default" = ["" "" ""];
+          };
+          "on-click" = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          "tooltip" = false;
+        };
+        "pulseaudio#microphone" = {
+          "format" = "{format_source}";
+          "format-source" = " {volume}%";
+          "format-source-muted" = " Muted";
+          "on-click" = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+          "on-scroll-up" = "wpctl set-volume -l 1.3 @DEFAULT_AUDIO_SOURCE@ 1%+";
+          "on-scroll-down" = "wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 1%-";
+        };
+
+        "clock" = {
+          "interval" = 1;
+          "format" = "{: %R   %d/%m}";
+          "tooltip" = true;
+          "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
+        "memory" = {
+          "interval" = 1;
+          "format" = "󰻠 {percentage}%";
+          "states" = {
+            "warning" = 85;
+          };
+        };
+        "cpu" = {
+          "interval" = 1;
+          "format" = "󰍛 {usage}%";
+        };
+        "mpd" = {
+          "max-length" = 25;
+          "format" = "<span foreground='#bb9af7'></span> {title}";
+          "format-paused" = " {title}";
+          "format-stopped" = "<span foreground='#bb9af7'></span>";
+          "format-disconnected" = "";
+          "on-click" = "mpc --quiet toggle";
+          "on-click-right" = "mpc update; mpc ls | mpc add";
+          "on-click-middle" = "kitty --class='ncmpcpp' ncmpcpp ";
+          "on-scroll-up" = "mpc --quiet prev";
+          "on-scroll-down" = "mpc --quiet next";
+          "smooth-scrolling-threshold" = 5;
+          "tooltip-format" = "{title} - {artist} ({elapsedTime:%M:%S}/{totalTime:%H:%M:%S})";
+        };
+        network = let
+          nm-editor = "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
+        in {
+          interface = "wlo1";
+          format-wifi = "󰖩 {essid} ";
+          format-ethernet = "󰈀";
+          format-alt = "󰤨  {bandwidthDownBytes}";
+          format-disconnected = "󰤭";
+          tooltip-format = "{ipaddr}/{ifname} via {gwaddr} ({signalStrength}%)";
+          on-click-right = "${nm-editor}";
+          interval = 1;
+        };
+        "custom/powermenu" = {
+          "format" = "";
+          "on-click" = "pkill wlogout || wlogout --protocol layer-shell";
+          "tooltip" = false;
+        };
+        "tray" = {
+          "icon-size" = 15;
+          "spacing" = 5;
+        };
+        "battery" = {
+          "states" = {
+            "good" = 95;
+            "warning" = 20;
+            "critical" = 10;
+          };
+          "format" = "{icon} {capacity}%";
+          "format-charging" = " {capacity}%";
+          "format-plugged" = " {capacity}%";
+          "format-alt" = "{time} {icon}";
+          "format-icons" = [
+            " "
+            " "
+            " "
+            " "
+          ];
+        };
+      }
+    ];
+  };
+}
