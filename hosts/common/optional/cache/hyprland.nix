7d793de2892942ec38c53836f5d2aec71db0d669
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-17 09:54:58 +0100

    add cache entry of hyprland

---
+++ hosts/common/optional/cache/hyprland.nix
@@ -0,0 +1,12 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
+}
--- hosts/common/optional/gui/hyprland.nix
+++ hosts/common/optional/gui/hyprland.nix
@@ -3,8 +3,21 @@
   pkgs,
   ...
 }: {
+  # Fix wayland for electron based apps
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  hardware.brillo.enable = true;
+
+  services.gvfs.enable = true;
+
+  # Piper service (logitech mouse)
+  services.ratbagd.enable = true;
+
+  services.upower.enable = true;
+
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
+
+  security.pam.services.hyprlock = {};
 }
