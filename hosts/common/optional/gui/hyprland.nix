96fe229a4df0ab1439ce86f793a726cec283fb3e
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-17 09:56:22 +0100

    add cache entry of hyprland

--- hosts/common/optional/gui/hyprland.nix
+++ hosts/common/optional/gui/hyprland.nix
@@ -3,6 +3,9 @@
   pkgs,
   ...
 }: {
+  imports = [
+    ../cache/hyprland.nix
+  ];
   # Fix wayland for electron based apps
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   hardware.brillo.enable = true;
