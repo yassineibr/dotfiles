dd577c1316247c057e2d64886a165eca9be79dd4
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-17 01:10:01 +0100

    add tailscale config (nixos)

---
+++ hosts/common/optional/network/tailscale.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = lib.mkDefault "client";
+  };
+  # networking.nftables.enable = true;
+  networking.firewall = {
+    checkReversePath = "loose";
+    trustedInterfaces = ["tailscale0"];
+    allowedUDPPorts = [config.services.tailscale.port];
+  };
+}
