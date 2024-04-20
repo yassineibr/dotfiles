5489960cce52eb92256530c27c66903e1555829f
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-20 13:34:39 +0100

    add outputs to hosts config (used for overlays)

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,19 +1,20 @@
 {
   lib,
   inputs,
+  outputs,
   ...
 }: {
   # MSI Laptop
   alpha = lib.nixosSystem {
     system = "x86_64-linux";
-    specialArgs = {inherit inputs;};
+    specialArgs = {inherit inputs outputs;};
     modules = [];
   };
 
   # HP Laptop
   beta = lib.nixosSystem {
     system = "x86_64-linux";
-    specialArgs = {inherit inputs;};
+    specialArgs = {inherit inputs outputs;};
     modules = [
       ./beta
     ];
