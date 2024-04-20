1fa16ee0a66ca1228c05315495f9d76d01079849
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-20 22:35:43 +0100

    add new personal nix cache (nixos)

--- hosts/common/optional/cache/default.nix
+++ hosts/common/optional/cache/default.nix
@@ -4,9 +4,10 @@
   ...
 }: {
   nix.settings = {
-    substituters = ["https://nix-community.cachix.org"];
+    substituters = ["https://nix-community.cachix.org" "https://cache.ibrahimi.xyz"];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.ibrahimi.xyz:sJXcKBgYznXb0me/fndlO2l470PTBjeuNrV7/v5Fguk="
     ];
   };
 }
