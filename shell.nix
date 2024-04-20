697e3d93cc2ae31933ad2995676d13d137e7eb29
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-20 13:33:19 +0100

    add unstable overlay to shell.nix (nix-shell)

--- shell.nix
+++ shell.nix
@@ -3,14 +3,24 @@
   pkgs ?
   # If pkgs is not defined, instanciate nixpkgs from locked commit
   let
-    lock =
-      (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
-    nixpkgs = fetchTarball {
+    getLock = flake: (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.${flake}.locked;
+    getTarBall = lock: fetchTarball {
       url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
       sha256 = lock.narHash;
     };
+    nixpkgs = getTarBall (getLock "nixpkgs");
+    nixpkgs-unstable = getTarBall (getLock "nixpkgs-unstable");
   in
-    import nixpkgs {overlays = [];},
+    import nixpkgs {
+      overlays = [
+        (final: _prev: {
+          unstable = import nixpkgs-unstable {
+            system = final.system;
+            config.allowUnfree = true;
+          };
+        })
+      ];
+    },
   ...
 }: {
   default = pkgs.mkShell {
@@ -19,6 +29,7 @@
       git
       nix
       home-manager
+      unstable.nh
 
       # Secret management
       age
