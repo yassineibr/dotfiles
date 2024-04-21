e8fa3bc6db86ba9e501bb5638158a54ad1718ce1
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-21 15:01:42 +0100

    add lanzaboote for secureboot (nixos)

---
+++ hosts/common/optional/boot/secureboot.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  pkgs,
+  inputs,
+  lib,
+  ...
+}: {
+  imports = [inputs.lanzaboote.nixosModules.lanzaboote];
+
+  environment.systemPackages = [
+    # For debugging and troubleshooting Secure Boot.
+    pkgs.sbctl
+  ];
+
+  # Lanzaboote currently replaces the systemd-boot module.
+  # This setting is usually set to true in configuration.nix
+  # generated at installation time. So we force it to false
+  # for now.
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  # boot.loader.timeout = 5;
+
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+    settings = {
+      timeout = 3;
+      auto-firmware = false;
+    };
+  };
+  ## Secure Boot
+  boot.bootspec.enable = true;
+}
