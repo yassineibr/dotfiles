781bcfb93d85e34aab1f7f46f4a2d6e67289208b
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-21 15:46:26 +0100

    add nftable firewall (nixos)

---
+++ hosts/common/optional/firewall.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  pkgs,
+  inputs,
+  lib,
+  ...
+}: {
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [1234];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+  networking.nftables.enable = true;
+}
