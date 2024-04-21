c3e980b3b71bc764a4aab5c742e50fb4269cd158
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-21 16:02:14 +0100

    add printer module (nixos)

---
+++ hosts/common/optional/services/printer.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  pkgs,
+  inputs,
+  lib,
+  ...
+}: {
+  # Enable CUPS to print documents.
+  services.printing = {
+    enable = true;
+    drivers = [pkgs.hplipWithPlugin];
+  };
+
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+
+  # for a WiFi printer
+  # services.avahi.openFirewall = true;
+}
