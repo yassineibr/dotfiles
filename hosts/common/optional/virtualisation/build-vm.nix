a88db4ae5442c76fa3893f6ea33dc9b86a501de4
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-21 14:33:19 +0100

    add build-vm config (nixos)

---
+++ hosts/common/optional/virtualisation/build-vm.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  config,
+  ...
+}: {
+  virtualisation.vmVariant = {
+    users.users.yassine = {
+      initialPassword = "test";
+    };
+    users.groups.test = {};
+
+    virtualisation = {
+      memorySize = 8192;
+      cores = 2;
+
+      qemu.options = [
+        "-device virtio-vga-gl"
+        "-display sdl,gl=on,show-cursor=off"
+        "-audio pa,model=hda"
+      ];
+    };
+
+    environment.sessionVariables = {WLR_NO_HARDWARE_CURSORS = "1";};
+
+    services.interception-tools.enable = lib.mkForce false;
+  };
+}
