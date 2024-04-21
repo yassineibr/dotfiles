bd5adad6eb52af3c008dac7bfc874317f06a6db5
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-21 15:59:41 +0100

    add cron notification (nixos)

---
+++ hosts/common/optional/health.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+  services.cron = {
+    enable = true;
+    systemCronJobs = with pkgs; [
+      ''
+        */5 * * * *      yassine  DISPLAY=':1' DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"  ${dunst}/bin/dunstify -t 20000 "Health" "Look away from the screen"
+      ''
+    ];
+  };
+}
