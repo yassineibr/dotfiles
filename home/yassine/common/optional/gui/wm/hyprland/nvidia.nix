6c053790175bcd7bfed3229d53ff3615e4a95fb8
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-21 14:49:54 +0100

    add nvidia env for Hyprland (home-manager)

---
+++ home/yassine/common/optional/gui/wm/hyprland/nvidia.nix
@@ -0,0 +1,17 @@
+{
+  inputs,
+  pkgs,
+  config,
+  ...
+}: {
+  wayland.windowManager.hyprland = {
+    settings = {
+      env = [
+        "LIBVA_DRIVER_NAME,nvidia"
+        "GBM_BACKEND,nvidia-drm"
+        "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+        # "WLR_DRM_DEVICES,/dev/dri/card1:/dev/dri/card0"
+      ];
+    };
+  };
+}
