1b9673ac92caf11a1187692526a9052497bda41e
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-04-22 12:12:34 +0100

    add beta pub ssh key (nixos)

--- hosts/common/users/yassine/default.nix
+++ hosts/common/users/yassine/default.nix
@@ -25,6 +25,9 @@ in {
         "libvirtd"
         "vboxusers"
       ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqgfTS1lt6os0E7w9iyClSvlXSapzQBtIV9wyI9gnK1 yassine@beta"
+    ];
   };
 
   home-manager.users.yassine = import ../../../../home/yassine/${config.networking.hostName}.nix;
