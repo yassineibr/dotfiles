8702cacece0f0fceed94a031679165f0845578cd
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-05-03 12:04:48 +0100

    add eww bar WIP (home-manager)

---
+++ home/yassine/common/optional/gui/bars/eww/default.nix
@@ -0,0 +1,47 @@
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  hypr_sock = "$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock";
+in {
+  programs.eww = {
+    enable = true;
+    package = pkgs.eww-wayland;
+    configDir = ./bar;
+  };
+  home.packages = [
+    pkgs.jq
+
+    (pkgs.writeShellScriptBin "eww-change-active-workspace" ''
+      direction=$1
+      current=$2
+      if test "$direction" = "down"
+      then
+        hyprctl dispatch workspace e-1
+      elif test "$direction" = "up"
+      then
+        hyprctl dispatch workspace e+1
+      fi
+    '')
+
+    (pkgs.writeShellScriptBin "eww-get-active-workspace" ''
+      hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id'
+
+      ${lib.getExe pkgs.socat} -u UNIX-CONNECT:${hypr_sock} - |
+        stdbuf -o0 awk -F '>>|,' -e '/^workspace>>/ {print $2}' -e '/^focusedmon>>/ {print $3}'
+    '')
+
+    (pkgs.writeShellScriptBin "eww-get-workspaces" ''
+      spaces (){
+      	WORKSPACE_WINDOWS=$(hyprctl workspaces -j | jq 'map({key: .id | tostring, value: .windows}) | from_entries')
+      	seq 1 10 | jq --argjson windows "$WORKSPACE_WINDOWS" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
+      }
+
+      spaces
+      ${lib.getExe pkgs.socat} -u UNIX-CONNECT:${hypr_sock} - | while read -r line; do
+      	spaces
+      done
+    '')
+  ];
+}
