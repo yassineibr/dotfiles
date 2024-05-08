fc207088c021d78ae54c67d82ef3ce88fcb721e9
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-05-08 23:22:35 +0100

    update secrets, declare user password

--- hosts/common/core/sops.nix
+++ hosts/common/core/sops.nix
@@ -7,7 +7,7 @@
   # This will add secrets.yml to the nix store
   # You can avoid this by adding a string to the full path instead, i.e.
   # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
-  sops.defaultSopsFile = ../../../secrets.yml;
+  sops.defaultSopsFile = ../secrets.yml;
   # This will automatically import SSH keys as age keys
   sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
   # This is using an age key that is expected to already be in the filesystem
--- hosts/common/users/yassine/default.nix
+++ hosts/common/users/yassine/default.nix
@@ -1,11 +1,11 @@
 {
   pkgs,
-  inputs,
   config,
   ...
 }: let
   ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 in {
+  users.mutableUsers = false;
   users.users.yassine = {
     isNormalUser = true;
     description = "yassine";
@@ -24,6 +24,12 @@ in {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqgfTS1lt6os0E7w9iyClSvlXSapzQBtIV9wyI9gnK1 yassine@beta"
     ];
+    hashedPasswordFile = config.sops.secrets.yassine-password.path;
+  };
+
+  sops.secrets.yassine-password = {
+    sopsFile = ../../secrets.yml;
+    neededForUsers = true;
   };
 
   home-manager.users.yassine = import ../../../../home/yassine/${config.networking.hostName}.nix;
