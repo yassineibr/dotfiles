7af81602d344c3d2ef38909d56b008160de4576f
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-05-09 12:00:36 +0100

    treefmt use the nixfmt-rfc-style instead of alejandra

--- shell/treefmt.nix
+++ shell/treefmt.nix
@@ -1,10 +1,13 @@
 # Tree wide Formatting just run "nix fmt"
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   # Used to find the project root
   projectRootFile = "flake.nix";
 
   # Format nix files
-  programs.alejandra.enable = true;
+  # programs.alejandra.enable = true;
+  programs.nixfmt-rfc-style.enable = true;
+  programs.nixfmt-rfc-style.package = pkgs.unstable.nixfmt-rfc-style;
 
   # Format markdown files
   programs.mdformat.enable = true;
