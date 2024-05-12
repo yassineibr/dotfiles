a9c10342fc296f740b7787ef2f5662aab31e8df4
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-05-13 00:24:09 +0100

    fix: remove duplicate patch

--- hosts/omega/wol.nix
+++ hosts/omega/wol.nix
@@ -6,13 +6,10 @@
 }:
 
 let
-  alx-kernel-module = config.boot.kernelPackages.callPackage ../../pkgs/alx/alx.nix { };
-  patched = alx-kernel-module.overrideAttrs (prev: {
-    patches = [ ../../pkgs/alx/linux-6.1.patch ];
-  });
+  alx-pkm = config.boot.kernelPackages.callPackage ../../pkgs/alx/alx.nix { };
 in
 {
   networking.interfaces.enp3s0.wakeOnLan.enable = true;
 
-  boot.extraModulePackages = [ (lib.hiPrio patched) ];
+  boot.extraModulePackages = [ (lib.hiPrio alx-pkm) ];
 }
