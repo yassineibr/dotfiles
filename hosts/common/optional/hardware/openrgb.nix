1a9be7ec97f4c2624944d91ab183194206890535
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-05-29 00:43:43 +0100

    feat: add openrgb service

---
+++ hosts/common/optional/hardware/openrgb.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+let
+  no-rgb = pkgs.writeScriptBin "no-rgb" ''
+    #!/bin/sh
+    NUM_DEVICES=$(${pkgs.openrgb}/bin/openrgb --noautoconnect --list-devices | grep -E '^[0-9]+: ' | wc -l)
+
+    for i in $(seq 0 $(($NUM_DEVICES - 1))); do
+      ${pkgs.openrgb}/bin/openrgb --noautoconnect --device $i --mode static --color 000000
+    done
+  '';
+in
+{
+  # boot.kernelModules = ["i2c-dev" "i2c-i801" "i2c-nct6775"];
+  config = {
+    services.udev.packages = [ pkgs.openrgb ];
+    services.hardware.openrgb.enable = true;
+
+    boot.kernelModules = [ "i2c-dev" ];
+    hardware.i2c.enable = true;
+
+    systemd.services.no-rgb = {
+      description = "no-rgb";
+      serviceConfig = {
+        ExecStart = "${no-rgb}/bin/no-rgb";
+        Type = "oneshot";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
+}
