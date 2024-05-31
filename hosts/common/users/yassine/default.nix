ddc4900c747ae6db6dfd8075183e52e7b798c76a
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-05-31 12:11:49 +0100

    feat: add ssh public key

--- hosts/common/users/yassine/default.nix
+++ hosts/common/users/yassine/default.nix
@@ -20,7 +20,8 @@ in
         "vboxusers"
       ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqgfTS1lt6os0E7w9iyClSvlXSapzQBtIV9wyI9gnK1 yassine@beta"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqgfTS1lt6os0E7w9iyClSvlXSapzQBtIV9wyI9gnK1 yassine"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItiOURWobhCmj8SErugcEBJyhSd7V1Eqcm4u8wptfRY yassine"
     ];
     hashedPasswordFile = config.sops.secrets.yassine-password.path;
   };
