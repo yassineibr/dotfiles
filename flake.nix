570cfdb98342fbe0d30abdaeafcfdcc7bda4e4be
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-07-25 16:08:16 +0100

    testing unstable

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
--- hosts/common/optional/hardware/nvidia/laptop.nix
+++ hosts/common/optional/hardware/nvidia/laptop.nix
@@ -3,7 +3,7 @@
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
-    driSupport = true;
+    # driSupport = true;
     driSupport32Bit = true;
   };
 
