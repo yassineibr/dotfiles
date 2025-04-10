455adb68154e6018942e3513f9c5d43e21cbaf2f
yassineibr <75940849+yassineibr@users.noreply.github.com>
2025-04-10 15:11:02 +0100

    chore: update netbird to 0.40.1

--- overlays/default.nix
+++ overlays/default.nix
@@ -22,13 +22,13 @@
     };
 
     netbird = _prev.netbird.overrideAttrs (oldAttrs: rec {
-      version = "0.40.0";
+      version = "0.40.1";
 
       src = _prev.fetchFromGitHub {
         owner = "netbirdio";
         repo = "netbird";
         rev = "v${version}";
-        sha256 = "sha256-GbKA6tJLCQNCiG9rj3iW4l51nQEbt42u7B6tFCbDSTQ=";
+        sha256 = "sha256-bfw0GjYyfDFrt3ggXl6kR52L4+zdf2J2x/waTTIUsIw=";
       };
 
       vendorHash = "sha256-vy725OvkYLyCDYEmnPpXJWqyofb29GiP4GkLn1GInm0=";
