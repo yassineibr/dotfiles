c44c8d0aef8b2903e5f7320a2ff244f8bb2af943
yassineibr <75940849+yassineibr@users.noreply.github.com>
2025-04-14 15:38:49 +0100

    netbird: update to 0.41.0

--- overlays/default.nix
+++ overlays/default.nix
@@ -22,13 +22,13 @@
     };
 
     netbird = _prev.netbird.overrideAttrs (oldAttrs: rec {
-      version = "0.40.1";
+      version = "0.41.0";
 
       src = _prev.fetchFromGitHub {
         owner = "netbirdio";
         repo = "netbird";
         rev = "v${version}";
-        sha256 = "sha256-bfw0GjYyfDFrt3ggXl6kR52L4+zdf2J2x/waTTIUsIw=";
+        sha256 = "sha256-xVk5vAU9G4HTaHmK1i8HvvYUxN0B2T2w+Vj4Ypg7N4s=";
       };
 
       vendorHash = "sha256-vy725OvkYLyCDYEmnPpXJWqyofb29GiP4GkLn1GInm0=";
