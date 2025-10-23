c20d8163b7b8210f9e3a6f4cdb675c88931d39b7
yassineibr <75940849+yassineibr@users.noreply.github.com>
2025-10-23 20:29:15 +0100

    netbird: update to 0.59.8

--- overlays/_sources/generated.nix
+++ overlays/_sources/generated.nix
@@ -8,13 +8,13 @@
 {
   netbird = {
     pname = "netbird";
-    version = "v0.59.7";
+    version = "v0.59.8";
     src = fetchFromGitHub {
       owner = "netbirdio";
       repo = "netbird";
-      rev = "v0.59.7";
+      rev = "v0.59.8";
       fetchSubmodules = false;
-      sha256 = "sha256-qOb4viLMZme6sJzWxJuYkubQ3D67j5gkkLTRkcL2QKI=";
+      sha256 = "sha256-dQzLmGOrimbygmLAJYZn6HG7Dg3wu5gajv5eOL9bk6w=";
     };
     vendorHash = "sha256-JMynPCH5Zvzz8nYI7DPu9TzU+rWJemPwyDdlPkFiqds=";
   };
