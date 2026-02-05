a9af0e80794af151b9c4c0972cad0e849d70629a
Yassine IBRAHIMI <75940849+yassineibr@users.noreply.github.com>
2026-02-06 00:33:46 +0100

    Update nixConfig with additional substituters and keys
    Add extra substituters and trusted public keys to nixConfig

--- flake.nix
+++ flake.nix
@@ -1,4 +1,17 @@
 {
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+      "https://attic.ibrahimi.xyz/system"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "system:51hbrsnX2pmCB3z3otgdnt2aq+konejsg/bQ7Qu1W+o="
+    ];
+  };
+
   description = "NixOS/Home-Manager Config Flake";
 
   inputs = {
